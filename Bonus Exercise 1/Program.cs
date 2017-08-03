c068c1d1a1bf292da8d1f5840c3fe9b06f81069b
Ariana-W <ariana.waller@yahoo.com>
2017-08-03 12:46:13 -0400

    Initial Commit

---
+++ Bonus Exercise 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Dieonte!");
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter a string");
+            string useroutput =  Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine($"You just entered: {useroutput} ");
+        }
+    }
+}
